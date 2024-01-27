654680ca50caff2476230ef57781f6f753f25bf4
Jakub Beránek <jakub.beranek@vsb.cz>
2024-01-27 19:32:36 +0100

    Add environment variable setting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use clap::Parser;
 use std::path::Path;
 use std::process::Command;
 use std::time::Instant;
-use clap::Parser;
 
-fn make_commands(count: usize, binary: &str) -> Vec<Command> {
+fn make_commands(count: usize, binary: &str, set_env: bool) -> Vec<Command> {
     std::iter::from_fn(|| {
         let mut cmd = Command::new(binary);
         cmd.arg("0");
+        if set_env {
+            cmd.env("FOO", "BAR");
+        }
         Some(cmd)
     })
     .take(count)
@@ -27,7 +30,9 @@ struct Args {
     #[arg(long)]
     alloc: Option<u64>,
     #[arg(long, default_value = "/usr/bin/sleep")]
-    binary: String
+    binary: String,
+    #[arg(long)]
+    set_env: bool,
 }
 
 fn main() {
@@ -49,9 +54,7 @@ fn main() {
 
     let mut allocated: Vec<u8> = vec![];
     let allocs = match args.alloc {
-        Some(alloc) => {
-            (0..5).map(|i| alloc * 1024 * 1024).collect()
-        }
+        Some(alloc) => (0..5).map(|i| alloc * 1024 * 1024).collect(),
         None => {
             vec![0]
         }
@@ -61,44 +64,56 @@ fn main() {
     let modes = ["spawn"];
     // let modes = ["spawn", "wait"];
     let counts = if allocs.len() == 1 {
-        vec![1000, 5000, 10000, 25000]
+        vec![10000] //, 5000, 10000, 25000]
     } else {
         vec![10000]
     };
+    let env_increases = vec![50].repeat(5);
+    let mut env_count = std::env::vars_os().count();
 
     let repeat_count = 3;
-    let env_count = std::env::vars_os().count();
 
     for alloc in allocs {
         allocated.resize(allocated.len() + alloc as usize, alloc as u8);
 
-        for process_count in &counts {
-            for mode in modes {
-                for _ in 0..repeat_count {
-                    assert!(children.is_empty());
-                    let commands = make_commands(*process_count, &args.binary);
+        for env_incr in &env_increases {
+            for _ in 0..*env_incr {
+                std::env::set_var(format!("KEY_ENV_TEST_{}", env_count), "X".repeat(25));
+                env_count += 1;
+            }
 
-                    let start = Instant::now();
-                    match mode {
-                        "spawn" => {
-                            for mut command in commands {
-                                let child = command.spawn().unwrap();
-                                children.push(child);
+            for process_count in &counts {
+                for mode in modes {
+                    for _ in 0..repeat_count {
+                        assert!(children.is_empty());
+                        let commands = make_commands(*process_count, &args.binary, args.set_env);
+
+                        let start = Instant::now();
+                        match mode {
+                            "spawn" => {
+                                for mut command in commands {
+                                    let child = command.spawn().unwrap();
+                                    children.push(child);
+                                }
                             }
-                        }
-                        "wait" => {
-                            for mut command in commands {
-                                command.spawn().unwrap().wait().unwrap();
+                            "wait" => {
+                                for mut command in commands {
+                                    command.spawn().unwrap().wait().unwrap();
+                                }
                             }
+                            _ => panic!(),
                         }
-                        _ => panic!()
-                    }
 
-                    let duration = start.elapsed().as_secs_f64();
-                    println!("{name},{process_count},{env_count},{mode},{},{duration}", allocated.len());
+                        let duration = start.elapsed().as_secs_f64();
+                        assert_eq!(std::env::vars_os().count(), env_count);
+                        println!(
+                            "{name},{process_count},{env_count},{mode},{},{duration}",
+                            allocated.len()
+                        );
 
-                    for mut child in children.drain(..) {
-                        child.wait().unwrap();
+                        for mut child in children.drain(..) {
+                            child.wait().unwrap();
+                        }
                     }
                 }
             }
