5f8ea1f9c0814bdbf017969ba78aba1fca53e2f9
Jakub Beránek <jakub.beranek@vsb.cz>
2024-01-27 10:34:11 +0100

    Configure vfork in C++ benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -18,7 +18,7 @@
 int main(int argc, char** argv) {
     std::vector<uint8_t> allocated;
 
-    if (argc < 3) {
+    if (argc < 4) {
         return 1;
     }
 
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
     std::vector<int> counts = {1000, 5000, 10000, 25000};
     int repeat_count = 3;
 
-    std::cout << "name,process_count,env_count,mode,allocated,duration" << std::endl;
+    std::cout << "name,process_count,env_count,mode,vfork,allocated,duration" << std::endl;
 
     std::vector<int> alloc_increments;
     if (alloc == 0) {
@@ -40,45 +40,53 @@ int main(int argc, char** argv) {
         counts = {10000};
     }
 
+    std::vector<bool> vfork_modes = {false, true};
+
     std::vector<pid_t> processes;
     for (auto alloc_increment: alloc_increments) {
         allocated.resize(allocated.size() + (alloc_increment * 1024 * 1024));
 
-        for (const auto& process_count: counts) {
-            for (int i = 0; i < repeat_count; i++) {
-                processes.clear();
+        for (auto use_vfork: vfork_modes) {
+            for (const auto& process_count: counts) {
+                for (int i = 0; i < repeat_count; i++) {
+                    processes.clear();
 
-                auto start = std::chrono::system_clock::now();
-                for (int p = 0; p < process_count; p++) {
-                    pid_t pid;
+                    auto start = std::chrono::system_clock::now();
+                    for (int p = 0; p < process_count; p++) {
+                        pid_t pid;
 
-                    char *envp[] = {NULL};
-                    char *args[] = {
-                        "/usr/bin/sleep",
-                        "0",
-                        NULL};
+                        char *envp[] = {NULL};
+                        char *args[] = {
+                            "/usr/bin/sleep",
+                            "0",
+                            NULL};
 
-                    posix_spawnattr_t attr;
-                    CHECK(posix_spawnattr_init(&attr));
-                    CHECK(posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSIGDEF | POSIX_SPAWN_SETSIGMASK/* | POSIX_SPAWN_USEVFORK*/));
+                        int flags = POSIX_SPAWN_SETSIGDEF | POSIX_SPAWN_SETSIGMASK;
+                        if (use_vfork) {
+                            flags |= POSIX_SPAWN_USEVFORK;
+                        }
+                        posix_spawnattr_t attr;
+                        CHECK(posix_spawnattr_init(&attr));
+                        CHECK(posix_spawnattr_setflags(&attr, flags));
 
-                    CHECK(posix_spawn(
-                        &pid,
-                        "/usr/bin/sleep",
-                        NULL,
-                        &attr,
-                        &args[0],
-                        &envp[0]));
-                    processes.push_back(pid);
-                    CHECK(posix_spawnattr_destroy(&attr));
-                }
-                auto end = std::chrono::system_clock::now();
-                auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-                double duration = static_cast<double>(duration_ms) / 1000.0;
-                std::cout << name << "-cpp," << process_count << ",1,spawn," << allocated.size() << "," << duration << std::endl;
+                        CHECK(posix_spawn(
+                            &pid,
+                            "/usr/bin/sleep",
+                            NULL,
+                            &attr,
+                            &args[0],
+                            &envp[0]));
+                        processes.push_back(pid);
+                        CHECK(posix_spawnattr_destroy(&attr));
+                    }
+                    auto end = std::chrono::system_clock::now();
+                    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+                    double duration = static_cast<double>(duration_ms) / 1000.0;
+                    std::cout << name << "-cpp," << process_count << ",1,spawn," << (use_vfork ? '1' : '0') << "," << allocated.size() << "," << duration << std::endl;
 
-                for (auto pid: processes) {
-                    waitpid(pid, NULL, 0);
+                    for (auto pid: processes) {
+                        waitpid(pid, NULL, 0);
+                    }
                 }
             }
         }
